51fa05fa7e36572e2aa4ffa420b9c10251d6d768
Giovanni Francesco Comune <giovanni.francesco11c@gmail.com>
2023-08-28 17:58:43 +0200

    fixed SHELL03 not using custom commands

--- RT-STM32G474RE-NUCLEO64-SHELL03/main.c
+++ RT-STM32G474RE-NUCLEO64-SHELL03/main.c
@@ -1,10 +1,13 @@
 /*
-    NeaPolis Innovation Summer Campus Examples
+    NeaPolis Innovation Summer Campus 2023 Examples
     Copyright (C) 2020-2023 Salvatore Dello Iacono [delloiaconos@gmail.com]
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
         http://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,18 +16,22 @@
 */
 
 /*
- * [NISC2023-BASE] - Basic Project Structure.
- * DESCRIPTION: This project has been adapted from the ChibiOS Demos
+ * [NISC2023-SHELL03] - Using the shell as a menu.
+ * DESCRIPTION: Single shell thread with a runtime defined configuration.
  */
 
 #include "ch.h"
 #include "hal.h"
 
-#include "shell.h"
 #include "chprintf.h"
+#include "shell.h"
+#include "shellcommands.h"
 
 
-#include <string.h>
+static ShellConfig shell_cfg = {
+  (BaseSequentialStream *)&SD2,
+  (ShellCommand *) NULL
+};
 
 static void cmd_hello(BaseSequentialStream *chp, int argc, char *argv[]) {
   (void) argc;
@@ -33,43 +40,37 @@ static void cmd_hello(BaseSequentialStream *chp, int argc, char *argv[]) {
   chprintf(chp, "Hello World!\r\n");
 }
 
-static void cmd_led(BaseSequentialStream *chp, int argc, char *argv[]) {
-  if( argc == 1 && strcmp( argv[0], "ON" ) == 0 ) {
-    palSetLine( LINE_LED_GREEN );
-  } else if( argc == 1 && strcmp( argv[0], "OFF" ) == 0 ) {
-    palClearLine( LINE_LED_GREEN );
-  } else {
-    chprintf(chp, "Usage: led [ON|OFF]\r\n");
-  }
-}
-
-static const ShellCommand commands[] = {
+static const ShellCommand cmds0[] = {
+  {"menu",  cmd_menu},
+  {"menu1", cmd_menu1},
+  {"menu2", cmd_menu2},
   {"hello", cmd_hello},
-  {"led", cmd_led},
   {NULL, NULL}
 };
 
-static const ShellConfig shell_cfg1 = {
-  (BaseSequentialStream *)&SD2,
-  commands
+static const ShellCommand cmds1[] = {
+  {"menu", cmd_menu},
+  {"back", cmd_menu0},
+  {"say",  cmd_say},
+  {NULL, NULL}
 };
+
+static const ShellCommand cmds2[] = {
+  {"menu", cmd_menu},
+  {"back", cmd_menu0},
+  {"tell", cmd_tell},
+  {NULL, NULL}
+};
+
 
 #define WA_SHELL   2048
 THD_WORKING_AREA( waShell, WA_SHELL );
-
 
 /*
  * Application entry point.
  */
 int main(void) {
 
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
   halInit();
   chSysInit();
 
@@ -86,12 +87,27 @@ int main(void) {
    */
   shellInit();
 
-  /*
-   * Create Shell Thread!
-   */
-  chThdCreateStatic(waShell, sizeof(waShell), NORMALPRIO, shellThread, (void *)&shell_cfg1);
+  shell_cfg.sc_commands = cmds0;
 
   while (true) {
-    chThdSleepMilliseconds(1000);
+    thread_t * tp;
+    msg_t msg;
+
+    tp = chThdCreateStatic(waShell, sizeof(waShell), NORMALPRIO, shellThread, (void *)&shell_cfg);
+    msg = chThdWait( tp );
+
+    switch( msg ) {
+    case MSG_OK+1:
+      shell_cfg.sc_commands = cmds1;
+      break;
+    case MSG_OK+2:
+      shell_cfg.sc_commands = cmds2;
+      break;
+    default:
+    case MSG_OK:
+      shell_cfg.sc_commands = cmds0;
+    }
+
+    chThdSleepMilliseconds(100);
   }
 }
