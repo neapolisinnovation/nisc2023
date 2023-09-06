3b7632079ef5f417b27ee85c696a1e732f221080
Salvatore Bramante <salvatore.bramante@yahoo.it>
2023-09-06 13:30:16 +0200

    BAD APPLE ROCKS

---
+++ RT-STM32G474RE-NUCLEO64-DAC01/main.c
@@ -0,0 +1,137 @@
+/*
+    Neapolis Innovation - Copyright (C) 2023 Salvatore Bramante, Giovanni Francesco Comune
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+
+
+#include "ch.h"
+#include "hal.h"
+#include "notes.h"
+
+#define PORTAB_LINE_LED1            LINE_LED
+#define PORTAB_LED_OFF              PAL_LOW
+#define PORTAB_LED_ON               PAL_HIGH
+#define PORTAB_LINE_BUTTON          LINE_BUTTON
+#define PORTAB_BUTTON_PRESSED       PAL_HIGH
+
+#define PORTAB_SD1                  LPSD1
+
+#define PORTAB_DAC_TRIG             7
+
+/**
+ *
+ * PA4: output DAC1
+ * PB7: trigger
+ *
+ * */
+
+
+/*
+ * DAC streaming callback.
+ */
+size_t nx = 0, ny = 0, nz = 0;
+static void end_cb1(DACDriver *dacp) {
+
+  nz++;
+  if (dacIsBufferComplete(dacp)) {
+    nx += DAC_BUFFER_SIZE / 4;
+  }
+  else {
+    ny += DAC_BUFFER_SIZE / 4;
+  }
+
+  if ((nz % 1000) == 0) {
+    palToggleLine(PORTAB_LINE_LED1);
+  }
+}
+
+/*
+ * DAC error callback.
+ */
+static void error_cb1(DACDriver *dacp, dacerror_t err) {
+
+  (void)dacp;
+  (void)err;
+
+  chSysHalt("DAC failure");
+}
+
+static const DACConfig dac1cfg1 = {
+  .init         = 2047U,
+  .datamode     = DAC_DHRM_12BIT_RIGHT,
+  .cr           = 0
+};
+
+static const DACConversionGroup dacgrpcfg1 = {
+  .num_channels = 1U,
+  .end_cb       = end_cb1,
+  .error_cb     = error_cb1,
+  .trigger      = DAC_TRG(PORTAB_DAC_TRIG)
+};
+
+/*
+ * GPT6 configuration.
+ */
+static GPTConfig gpt6cfg1 = {
+  .frequency    = 2*DAC_BUFFER_SIZE*vec[0],
+  .callback     = NULL,
+  .cr2          = TIM_CR2_MMS_1,    /* MMS = 010 = TRGO on Update Event.    */
+  .dier         = 0U
+};
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /* Setting up the output pin as analog as suggested
+       by the Reference Manual.*/
+  palSetPadMode(GPIOA, 4, PAL_MODE_INPUT_ANALOG);
+
+  /* Starting DAC1 driver.*/
+    dacStart(&DACD1, &dac1cfg1);
+
+    /* Starting GPT6 driver, it is used for triggering the DAC.*/
+    gptStart(&GPTD6, &gpt6cfg1);
+
+    int cont = 0;
+  while (true ) {
+	  while(true){
+	  if(cont ==BA_SIZE) break;
+  	  /* Starting a continuous conversion.*/
+  	    dacStartConversion(&DACD1, &dacgrpcfg1,
+  	                       (dacsample_t *)dac_buffer, DAC_BUFFER_SIZE);
+  	    gptStartContinuous(&GPTD6, 2U);
+        chThdSleepMilliseconds(pvec[cont] * 2.3 );
+        gptStopTimer(&GPTD6);
+        cont++;
+        gpt6cfg1.frequency= 2*DAC_BUFFER_SIZE*vec[cont];
+  	    gptStart(&GPTD6, &gpt6cfg1);
+	  }
+	  cont = 0;
+      gpt6cfg1.frequency= 2*DAC_BUFFER_SIZE*vec[cont];
+
+    }
+}
---
+++ RT-STM32G474RE-NUCLEO64-DAC01/notes.h
@@ -0,0 +1,160 @@
+#include <stdint.h>
+
+#define DAC_BUFFER_SIZE 360
+
+#define MIN(a,b) (((a)<(b))? (a):(b))
+
+
+#define BA_SIZE 53
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262 //188560
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1000000U
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1259921U
+#define NOTE_AS6 1865
+#define NOTE_B6  1414214U
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST 0
+
+/*
+ * DO = C
+ * RE = D
+ * MI = E
+ * FA = F
+ * SOL= G
+ * LA = A
+ * SI = B
+ */
+
+
+static const gptfreq_t vec[BA_SIZE] = {	NOTE_DS5, NOTE_F5, NOTE_FS5, NOTE_GS5, NOTE_AS5, NOTE_DS6, NOTE_CS6, NOTE_AS5,NOTE_DS5, NOTE_AS5, NOTE_GS5,NOTE_FS5, NOTE_F5,
+										NOTE_DS5, NOTE_F5, NOTE_FS5, NOTE_GS5, NOTE_AS5, NOTE_GS5, NOTE_FS5,
+										NOTE_F5, NOTE_DS5, NOTE_F5, NOTE_FS5, NOTE_F5, NOTE_DS5, NOTE_D5,NOTE_F5,
+										NOTE_DS5, NOTE_F5, NOTE_FS5, NOTE_GS5, NOTE_AS5, NOTE_DS6, NOTE_CS6, NOTE_AS5,NOTE_DS5, NOTE_AS5, NOTE_GS5,NOTE_FS5, NOTE_F5,
+										NOTE_DS5, NOTE_F5, NOTE_FS5, NOTE_GS5, NOTE_AS5, NOTE_GS5, NOTE_FS5,
+										NOTE_F5,NOTE_FS5, NOTE_GS5,  NOTE_AS5
+										};
+
+static const int pvec[BA_SIZE] = {	100, 100, 100, 100, 200, 100, 100, 200, 200, 100, 100, 100, 100,
+									100, 100, 100, 100, 200, 100, 100,
+									100, 100, 100, 100, 100, 100, 100, 100,
+									100, 100, 100, 100, 200, 100, 100, 200, 200, 100, 100, 100, 100,
+									100, 100, 100, 100, 200, 100, 100,
+									200, 200, 200, 200
+								};
+
+static const dacsample_t dac_buffer[DAC_BUFFER_SIZE] = {
+  2047, 2082, 2118, 2154, 2189, 2225, 2260, 2296, 2331, 2367, 2402, 2437,
+  2472, 2507, 2542, 2576, 2611, 2645, 2679, 2713, 2747, 2780, 2813, 2846,
+  2879, 2912, 2944, 2976, 3008, 3039, 3070, 3101, 3131, 3161, 3191, 3221,
+  3250, 3278, 3307, 3335, 3362, 3389, 3416, 3443, 3468, 3494, 3519, 3544,
+  3568, 3591, 3615, 3637, 3660, 3681, 3703, 3723, 3744, 3763, 3782, 3801,
+  3819, 3837, 3854, 3870, 3886, 3902, 3917, 3931, 3944, 3958, 3970, 3982,
+  3993, 4004, 4014, 4024, 4033, 4041, 4049, 4056, 4062, 4068, 4074, 4078,
+  4082, 4086, 4089, 4091, 4092, 4093, 4094, 4093, 4092, 4091, 4089, 4086,
+  4082, 4078, 4074, 4068, 4062, 4056, 4049, 4041, 4033, 4024, 4014, 4004,
+  3993, 3982, 3970, 3958, 3944, 3931, 3917, 3902, 3886, 3870, 3854, 3837,
+  3819, 3801, 3782, 3763, 3744, 3723, 3703, 3681, 3660, 3637, 3615, 3591,
+  3568, 3544, 3519, 3494, 3468, 3443, 3416, 3389, 3362, 3335, 3307, 3278,
+  3250, 3221, 3191, 3161, 3131, 3101, 3070, 3039, 3008, 2976, 2944, 2912,
+  2879, 2846, 2813, 2780, 2747, 2713, 2679, 2645, 2611, 2576, 2542, 2507,
+  2472, 2437, 2402, 2367, 2331, 2296, 2260, 2225, 2189, 2154, 2118, 2082,
+  2047, 2012, 1976, 1940, 1905, 1869, 1834, 1798, 1763, 1727, 1692, 1657,
+  1622, 1587, 1552, 1518, 1483, 1449, 1415, 1381, 1347, 1314, 1281, 1248,
+  1215, 1182, 1150, 1118, 1086, 1055, 1024,  993,  963,  933,  903,  873,
+   844,  816,  787,  759,  732,  705,  678,  651,  626,  600,  575,  550,
+   526,  503,  479,  457,  434,  413,  391,  371,  350,  331,  312,  293,
+   275,  257,  240,  224,  208,  192,  177,  163,  150,  136,  124,  112,
+   101,   90,   80,   70,   61,   53,   45,   38,   32,   26,   20,   16,
+    12,    8,    5,    3,    2,    1,    0,    1,    2,    3,    5,    8,
+    12,   16,   20,   26,   32,   38,   45,   53,   61,   70,   80,   90,
+   101,  112,  124,  136,  150,  163,  177,  192,  208,  224,  240,  257,
+   275,  293,  312,  331,  350,  371,  391,  413,  434,  457,  479,  503,
+   526,  550,  575,  600,  626,  651,  678,  705,  732,  759,  787,  816,
+   844,  873,  903,  933,  963,  993, 1024, 1055, 1086, 1118, 1150, 1182,
+  1215, 1248, 1281, 1314, 1347, 1381, 1415, 1449, 1483, 1518, 1552, 1587,
+  1622, 1657, 1692, 1727, 1763, 1798, 1834, 1869, 1905, 1940, 1976, 2012
+};
